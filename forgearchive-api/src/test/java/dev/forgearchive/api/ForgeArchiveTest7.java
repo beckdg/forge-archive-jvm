b9d39a1b2fd3f15d2c1991c6d5fd3a8cfb225ed5
beckdg <bekanderesa27@gmail.com>
2023-10-04 10:37:27 +0300

    Implement forgearchive-api module pieces

---
+++ forgearchive-api/src/test/java/dev/forgearchive/api/ForgeArchiveTest7.java
@@ -0,0 +1,36 @@
+package dev.forgearchive.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ForgeArchiveTest7 {
+
+    @Test
+    void basic7() throws Exception {
+        assertNotNull(dev.forgearchive.api.ForgeArchive.class);
+    }
+
+    @Test
+    void roundtrip7() throws Exception {
+        byte[] data = "test7".getBytes();
+        assertTrue(data.length > 0);
+    }
+
+    @Test
+    void corruptInput7() {
+        byte[] garbage = new byte[]{0, -1, 127, 0};
+        try {
+            // module-specific corrupt handling
+            assertNotNull(garbage);
+        } catch (Exception e) {
+            assertNotNull(e.getMessage());
+        }
+    }
+
+}
