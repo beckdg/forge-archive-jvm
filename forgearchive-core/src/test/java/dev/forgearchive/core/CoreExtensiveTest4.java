4e855ed186ce1d567e487ce919cce41b1aa6b561
beckdg <bekanderesa27@gmail.com>
2023-11-01 09:52:02 +0300

    Refine forgearchive-core module pieces

---
+++ forgearchive-core/src/test/java/dev/forgearchive/core/CoreExtensiveTest4.java
@@ -0,0 +1,48 @@
+package dev.forgearchive.core;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CoreExtensiveTest4 {
+
+    @Test
+    void testVarIntRoundtrip4() throws Exception {
+        int v = 509;
+        byte[] buf = new byte[10];
+        int off = VarInt.writeUnsigned(buf, 0, v);
+        BinaryReader r = BinaryReader.wrap(java.util.Arrays.copyOf(buf, off));
+        assertEquals(v, r.readVarInt());
+    }
+
+    @Test
+    void testChecksum4() {
+        byte[] data = new byte[5];
+        for (int j = 0; j < data.length; j++) data[j] = (byte) j;
+        long c = Checksum.crc32c(data);
+        assertTrue(c >= 0);
+    }
+
+    @Test
+    void testContentHash4() {
+        byte[] data = ("payload4").getBytes();
+        ContentHash h1 = ContentHash.sha256(data);
+        ContentHash h2 = ContentHash.sha256(data);
+        assertEquals(h1, h2);
+        assertEquals(64, h1.hex().length());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 4, 8, 16, 32})
+    void testHexRoundtrip4(int len) {
+        byte[] data = new byte[len];
+        for (int j = 0; j < len; j++) data[j] = (byte) (4 + j);
+        assertArrayEquals(data, Hex.decode(Hex.encode(data)));
+    }
+
+}
---
+++ forgearchive-core/src/test/java/dev/forgearchive/core/CoreExtensiveTest51.java
@@ -0,0 +1,48 @@
+package dev.forgearchive.core;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CoreExtensiveTest51 {
+
+    @Test
+    void testVarIntRoundtrip51() throws Exception {
+        int v = 6478;
+        byte[] buf = new byte[10];
+        int off = VarInt.writeUnsigned(buf, 0, v);
+        BinaryReader r = BinaryReader.wrap(java.util.Arrays.copyOf(buf, off));
+        assertEquals(v, r.readVarInt());
+    }
+
+    @Test
+    void testChecksum51() {
+        byte[] data = new byte[52];
+        for (int j = 0; j < data.length; j++) data[j] = (byte) j;
+        long c = Checksum.crc32c(data);
+        assertTrue(c >= 0);
+    }
+
+    @Test
+    void testContentHash51() {
+        byte[] data = ("payload51").getBytes();
+        ContentHash h1 = ContentHash.sha256(data);
+        ContentHash h2 = ContentHash.sha256(data);
+        assertEquals(h1, h2);
+        assertEquals(64, h1.hex().length());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 4, 8, 16, 32})
+    void testHexRoundtrip51(int len) {
+        byte[] data = new byte[len];
+        for (int j = 0; j < len; j++) data[j] = (byte) (51 + j);
+        assertArrayEquals(data, Hex.decode(Hex.encode(data)));
+    }
+
+}
