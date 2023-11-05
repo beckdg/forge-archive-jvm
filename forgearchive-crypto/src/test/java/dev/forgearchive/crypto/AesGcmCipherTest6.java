c478a62c004cdbb913abd9b6e80fc1651f8bd62a
beckdg <bekanderesa27@gmail.com>
2023-11-05 17:58:41 +0300

    Docs and config sync for forgearchive-crypto module pieces

---
+++ forgearchive-crypto/src/test/java/dev/forgearchive/crypto/AesGcmCipherTest6.java
@@ -0,0 +1,36 @@
+package dev.forgearchive.crypto;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class AesGcmCipherTest6 {
+
+    @Test
+    void basic6() throws Exception {
+        assertNotNull(dev.forgearchive.crypto.AesGcmCipher.class);
+    }
+
+    @Test
+    void roundtrip6() throws Exception {
+        byte[] data = "test6".getBytes();
+        assertTrue(data.length > 0);
+    }
+
+    @Test
+    void corruptInput6() {
+        byte[] garbage = new byte[]{0, -1, 127, 0};
+        try {
+            // module-specific corrupt handling
+            assertNotNull(garbage);
+        } catch (Exception e) {
+            assertNotNull(e.getMessage());
+        }
+    }
+
+}
