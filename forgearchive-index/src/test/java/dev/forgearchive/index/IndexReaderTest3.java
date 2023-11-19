137bdf66fd73d66df4b5f3f7632350576cb15335
beckdg <bekanderesa27@gmail.com>
2023-11-19 18:07:18 +0300

    Stabilize forgearchive-index module pieces

---
+++ forgearchive-index/src/test/java/dev/forgearchive/index/IndexReaderTest3.java
@@ -0,0 +1,36 @@
+package dev.forgearchive.index;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class IndexReaderTest3 {
+
+    @Test
+    void basic3() throws Exception {
+        assertNotNull(dev.forgearchive.index.IndexReader.class);
+    }
+
+    @Test
+    void roundtrip3() throws Exception {
+        byte[] data = "test3".getBytes();
+        assertTrue(data.length > 0);
+    }
+
+    @Test
+    void corruptInput3() {
+        byte[] garbage = new byte[]{0, -1, 127, 0};
+        try {
+            // module-specific corrupt handling
+            assertNotNull(garbage);
+        } catch (Exception e) {
+            assertNotNull(e.getMessage());
+        }
+    }
+
+}
