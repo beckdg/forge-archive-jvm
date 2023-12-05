c1378cf564105b847b33461fe54a35f3946847c9
beckdg <bekanderesa27@gmail.com>
2023-12-05 10:59:34 +0300

    Implement forgearchive-memory and related files

---
+++ forgearchive-memory/src/test/java/dev/forgearchive/memory/MemoryPoolTest4.java
@@ -0,0 +1,36 @@
+package dev.forgearchive.memory;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MemoryPoolTest4 {
+
+    @Test
+    void basic4() throws Exception {
+        assertNotNull(dev.forgearchive.memory.MemoryPool.class);
+    }
+
+    @Test
+    void roundtrip4() throws Exception {
+        byte[] data = "test4".getBytes();
+        assertTrue(data.length > 0);
+    }
+
+    @Test
+    void corruptInput4() {
+        byte[] garbage = new byte[]{0, -1, 127, 0};
+        try {
+            // module-specific corrupt handling
+            assertNotNull(garbage);
+        } catch (Exception e) {
+            assertNotNull(e.getMessage());
+        }
+    }
+
+}
