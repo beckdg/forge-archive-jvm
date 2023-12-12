18b4e59caa4f1de522c2ae3ab944fac4cad340ca
beckdg <bekanderesa27@gmail.com>
2023-12-12 09:40:03 +0300

    Integrate forgearchive-pack module pieces

---
+++ forgearchive-pack/src/test/java/dev/forgearchive/pack/ForgearchivePackCorruptTest0.java
@@ -0,0 +1,25 @@
+package dev.forgearchive.pack;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ForgearchivePackCorruptTest0 {
+
+    @Test
+    void corrupt0() {
+        byte[] bad = new byte[0];
+        try {
+            dev.forgearchive.core.BinaryReader r = dev.forgearchive.core.BinaryReader.wrap(bad);
+            r.readInt();
+        } catch (Exception e) {
+            assertNotNull(e);
+        }
+    }
+
+}
