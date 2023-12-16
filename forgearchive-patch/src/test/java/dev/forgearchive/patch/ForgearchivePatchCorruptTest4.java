a2f36b39beb631407e7985636238eb1efdb967d1
beckdg <bekanderesa27@gmail.com>
2023-12-16 10:31:13 +0300

    Introduce forgearchive-patch module pieces

---
+++ forgearchive-patch/src/test/java/dev/forgearchive/patch/ForgearchivePatchCorruptTest4.java
@@ -0,0 +1,25 @@
+package dev.forgearchive.patch;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ForgearchivePatchCorruptTest4 {
+
+    @Test
+    void corrupt4() {
+        byte[] bad = new byte[4];
+        try {
+            dev.forgearchive.core.BinaryReader r = dev.forgearchive.core.BinaryReader.wrap(bad);
+            r.readInt();
+        } catch (Exception e) {
+            assertNotNull(e);
+        }
+    }
+
+}
