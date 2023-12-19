3517f29277583a9213a13bf6d6d333402883e70e
beckdg <bekanderesa27@gmail.com>
2023-12-19 11:26:12 +0300

    Expand forgearchive-plugin and related files

---
+++ forgearchive-plugin/src/test/java/dev/forgearchive/plugin/PluginDescriptorTest1.java
@@ -0,0 +1,36 @@
+package dev.forgearchive.plugin;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class PluginDescriptorTest1 {
+
+    @Test
+    void basic1() throws Exception {
+        assertNotNull(dev.forgearchive.plugin.PluginDescriptor.class);
+    }
+
+    @Test
+    void roundtrip1() throws Exception {
+        byte[] data = "test1".getBytes();
+        assertTrue(data.length > 0);
+    }
+
+    @Test
+    void corruptInput1() {
+        byte[] garbage = new byte[]{0, -1, 127, 0};
+        try {
+            // module-specific corrupt handling
+            assertNotNull(garbage);
+        } catch (Exception e) {
+            assertNotNull(e.getMessage());
+        }
+    }
+
+}
---
+++ forgearchive-plugin/src/test/java/dev/forgearchive/plugin/PluginDescriptorTest2.java
@@ -0,0 +1,36 @@
+package dev.forgearchive.plugin;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class PluginDescriptorTest2 {
+
+    @Test
+    void basic2() throws Exception {
+        assertNotNull(dev.forgearchive.plugin.PluginDescriptor.class);
+    }
+
+    @Test
+    void roundtrip2() throws Exception {
+        byte[] data = "test2".getBytes();
+        assertTrue(data.length > 0);
+    }
+
+    @Test
+    void corruptInput2() {
+        byte[] garbage = new byte[]{0, -1, 127, 0};
+        try {
+            // module-specific corrupt handling
+            assertNotNull(garbage);
+        } catch (Exception e) {
+            assertNotNull(e.getMessage());
+        }
+    }
+
+}
